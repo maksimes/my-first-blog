8cf72ab59efed5914656ef1d08e1bdc52b1baa1b
maksimes <maksimes@mail.ru>
2019-02-03 00:59:09 +0300

    Added

---
+++ static/js/script.js
@@ -0,0 +1,13 @@
+var div_com = document.getElementById("div_comment");
+var resp_com = document.getElementById("resp_comment");
+resp_com.onclick = function() {
+    div_com.style.display = "block";
+    var exit_block = document.getElementById("exit_div");
+    exit_block.onclick = function() {
+    div_com.style.display = "none";
+}
+}
+var exit_block = document.getElementById("exit_div");
+exit_block.onclick = function() {
+    div_com.style.display = "none";
+}
