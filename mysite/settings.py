4d84486770eab86a894e5ffc9b59d0d2b519865d
maksimes <maksimes@mail.ru>
2019-02-15 23:12:00 +0300

    os.environ.get

--- mysite/settings.py
+++ mysite/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', '9jtmm$9a_ri*p2l187xptmysf&&iv&
 
 # SECURITY WARNING: don't run with debug turned on in production!
 #DEBUG = True
-DEBUG = bool( os.getenv('DJANGO_DEBUG', True) )
+DEBUG = bool( os.environ.get('DJANGO_DEBUG', True) )
 
 ALLOWED_HOSTS = ['maks-blog.herokuapp.com', '127.0.0.1', 'maksimes.pythonanywhere.com']
 
